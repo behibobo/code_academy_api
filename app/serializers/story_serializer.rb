3293ce443ca7de6f52b5b1ce849023864ea52c98
behzad <behzadfareed@gmail.com>
2020-12-04 13:05:04 +0330

    sss

---
+++ app/serializers/story_serializer.rb
@@ -0,0 +1,12 @@
+class StorySerializer < ActiveModel::Serializer
+  attributes :id, :body, :date, :time
+
+  def date
+    object.created_at.strftime("%Y-%m-%d")
+  end
+
+  def time
+    object.created_at.strftime("%H:%M:%S")
+  end
+  
+end
